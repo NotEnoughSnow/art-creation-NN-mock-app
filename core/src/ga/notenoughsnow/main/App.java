f1d118ec7f7d9f213ddf55b6f1d8368ec6f05b9f
Amin Mraidi <amine.mraidi@outlook.com>
2022-05-03 09:45:36 +0200

    added basic functionalities : shaperenderer

--- core/src/ga/notenoughsnow/main/App.java
+++ core/src/ga/notenoughsnow/main/App.java
@@ -1,19 +1,42 @@
 package ga.notenoughsnow.main;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class App extends ApplicationAdapter {
-	
+
+    public static OrthographicCamera camera;
+    public ShapeRenderer shapeRenderer;
 	@Override
 	public void create () {
+		camera = new OrthographicCamera(800,800);
+		camera.position.x = 400;
+		camera.position.y = 400;
+
+
+		shapeRenderer = new ShapeRenderer();
+		
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 1, 1, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
+		
+		camera.update();
+		 shapeRenderer.setProjectionMatrix(camera.combined);
+		 shapeRenderer.begin(ShapeType.Filled);
+		 shapeRenderer.setColor(Color.WHITE);
+		 shapeRenderer.line(0, 0, 0, 100, 100, 0);
+		 shapeRenderer.end();
+		 
+       camera.update();
+       
 	}
 	
 	@Override
