161d8b7ecbca05a0da284a44ecd51ae74535d48f
Amin Mraidi <amine.mraidi@outlook.com>
2022-05-03 04:23:52 +0200

    removed default batch and set a temp screen conf

--- desktop/src/ga/notenoughsnow/main/DesktopLauncher.java
+++ desktop/src/ga/notenoughsnow/main/DesktopLauncher.java
@@ -9,6 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
+		config.setWindowedMode(800, 800);
+		config.setResizable(false);
 		config.setTitle("Art Creation Mock App");
 		new Lwjgl3Application(new App(), config);
 	}
